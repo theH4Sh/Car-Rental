425d3dd25ae719ec7644c3429db5277c09cdc2dd
theH4Sh <syedhaseebnaqvi51214@gmail.com>
2025-05-24 00:57:55 +0500

    animations fixed

--- client/src/components/UserDropdown.jsx
+++ client/src/components/UserDropdown.jsx
@@ -27,7 +27,8 @@ const UserDropdown = ({ auth, mobile }) => {
     }, []);
 
     return (
-        <div ref={dropdownRef} className={`relative ${mobile ? 'flex' : 'hidden md:flex'} items-center space-x-3 px-2 py-2 rounded-full transition duration-150 hover:bg-gray-200`}>
+        <div ref={dropdownRef} 
+        className={`relative ${mobile ? 'flex' : 'hidden md:flex'} items-center space-x-3 px-2 py-2 rounded-full transition duration-150 hover:bg-gray-200`}>
             <div 
                 className="flex items-center space-x-2 cursor-pointer"
                 onClick={() => setOpen(!open)}
@@ -40,25 +41,23 @@ const UserDropdown = ({ auth, mobile }) => {
                 <ChevronDown className={`w-4 h-4 transition-transform duration-300 ${ open ? 'rotate-180' : ''}`} />
             </div>
 
-            { open && (
-                <div className="z-10 bg-white absolute right-0 mt-2 w-40 rounded-lg shadow-lg py-2 border border-gray-200 text-sm text-gray-600 pointer-events-none transition-all duration-300 ease-out opacity-100 scale-100">
-                    {mobile && (
-                        <div className={'py-2 border-b border-gray-200'}>
-                            <span className="flex justify-start items-center px-4 py-1">Signed in as</span>
-                            <span className="flex justify-start items-center px-4 py-1 font-bold">{auth.user}</span>
-                        </div>
-                    ) }
-                    <div className="border-b border-gray-200">
-                        <Link className="flex justify-start items-center px-4 py-2 gap-2 hover:bg-gray-100"><span><ShieldCheck className="w-4 h-4" /></span> Admin</Link>
-                        <Link className="flex justify-start items-center px-4 py-2 gap-2 hover:bg-gray-100"><span><User className="w-4 h-4" /></span> Profile</Link>
-                        <Link className="flex justify-start items-center px-4 py-2 gap-2 hover:bg-gray-100"><span><Settings className="w-4 h-4" /></span> Settings</Link>
+            <div className={`z-10 bg-white absolute right-0 top-full mt-2 w-40 rounded-lg shadow-lg py-2 border border-gray-200 text-sm text-gray-600 transition-all duration-300 ${ open ? 'ease-out opacity-100' : 'hidden opacity-0'}`}>
+                {mobile && (
+                    <div className={'py-2 border-b border-gray-200'}>
+                        <span className="flex justify-start items-center px-4 py-1">Signed in as</span>
+                        <span className="flex justify-start items-center px-4 py-1 font-bold">{auth.user}</span>
                     </div>
-                    <button onClick={handleLogout}
-                    className="w-full flex justify-start items-center px-4 py-2 gap-2 hover:bg-gray-100">
-                        <span><LogOut className="w-4 h-4" /></span>Logout
-                    </button>
-                </div> )
-            }
+                ) }
+                <div className="border-b border-gray-200">
+                    <Link className="flex justify-start items-center px-4 py-2 gap-2 hover:bg-gray-100"><span><ShieldCheck className="w-4 h-4" /></span> Admin</Link>
+                    <Link className="flex justify-start items-center px-4 py-2 gap-2 hover:bg-gray-100"><span><User className="w-4 h-4" /></span> Profile</Link>
+                    <Link className="flex justify-start items-center px-4 py-2 gap-2 hover:bg-gray-100"><span><Settings className="w-4 h-4" /></span> Settings</Link>
+                </div>
+                <button onClick={handleLogout}
+                className="w-full flex justify-start items-center px-4 py-2 gap-2 hover:bg-gray-100">
+                    <span><LogOut className="w-4 h-4" /></span>Logout
+                </button>
+            </div>
         </div>
     );
 }
