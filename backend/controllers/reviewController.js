f8e2c004651ac569d24f19027d647bbdee74ef1c
theH4Sh <syedhaseebnaqvi51214@gmail.com>
2025-05-30 01:00:42 +0500

    get and post review controller

--- backend/controllers/reviewController.js
+++ backend/controllers/reviewController.js
@@ -1,5 +1,52 @@
-const createReview = async (req, res) => {}
+const mongoose = require('mongoose')
+const Review = require('../models/Review') 
 
-const getReviews = async (req, res) => {}
+const createReview = async (req, res, next) => {
+    const { rating, comment } = req.body
+    const { carId } = req.params
+    const userId = req.user._id
+
+    if (!mongoose.Types.ObjectId.isValid(carId)) {
+        return res.status(400).json({ error: 'Invalid car ID' })
+    }
+
+    if (!Number.isInteger(rating)) {
+        return res.status(400).json({ error: "Rating must be an integer between 1 and 5"})
+    }
+    
+    try {
+        const review = new Review({
+            user: userId,
+            car: carId,
+            comment,
+            rating
+        })
+
+        await review.save()
+        res.status(201).json(review)
+    } catch (error) {
+        next(error)
+    }
+}
+
+const getReviews = async (req, res, next) => {
+    const { carId } = req.params
+
+    if (!mongoose.Types.ObjectId.isValid(carId)) {
+        return res.status(400).json({ error: 'Invalid car ID'})
+    }
+
+    try {
+        const reviews = await Review.find({ car: carId }).populate('user', 'username')
+
+        if (reviews.length == 0) {
+            return res.status(404).json({ error: 'No reviews'})
+        }
+
+        res.status(200).json({reviews})
+    } catch (error) {
+        next(error)
+    }
+}
 
 module.exports = { createReview, getReviews }
